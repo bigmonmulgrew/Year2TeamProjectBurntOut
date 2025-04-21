7c5f6080f810995fa117e1192e19792965cf6f8a
MutantGinga <haydenwilliamhefford@yahoo.com>
2025-04-21 15:08:16 +0100

    Added LoginController and User skeleton classes

---
+++ Scripts/AutoLoads/login_controller.gd
@@ -0,0 +1,9 @@
+extends Node
+
+var user_credentials : String
+
+func validate_user() -> void:
+	pass
+
+func authenticate() -> void:
+	pass
---
+++ Scripts/user.gd
@@ -0,0 +1,4 @@
+extends Node
+
+func input(event: InputEvent) -> void:
+	pass
