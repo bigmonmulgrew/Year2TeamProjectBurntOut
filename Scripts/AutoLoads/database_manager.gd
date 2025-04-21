40ce4c3b83927baed676f4e36a9bead32b9d0b10
David Mulgrew <bigmonmulgrew@users.noreply.github.com>
2025-04-21 14:49:12 +0100

    added skeleton for database manager

---
+++ Scripts/AutoLoads/database_manager.gd
@@ -0,0 +1,33 @@
+extends Node
+
+## Runtime Variables
+# DB connection
+var username: String
+var passwordHash: String
+# var instance: Database TODO not valid, need to check how to access SQL in godot
+
+# Scoring
+var fatigueScore: float
+var reactionTimeScore: float
+var memoryScore: float
+var focusScore: float
+var mentalAgility: float
+var decisionMakingScore: float
+
+# User
+var userFeedback: Array[int]
+var userAdditional: String
+var employeeTeams: Array[int]
+var employeeFatigueScore: int
+
+func query_user() -> void:
+	pass
+
+func store_data() -> void:
+	pass
+
+func find_valid_feedback() -> void:
+	pass
+
+func find_valid_fatigue_score() -> void:
+	pass
