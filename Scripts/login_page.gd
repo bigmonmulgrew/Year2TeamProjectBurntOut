17fe57aa43b26be9939737201ae953b1e05ae427
David Mulgrew <bigmonmulgrew@users.noreply.github.com>
2025-04-23 09:38:00 +0100

    added progress bar and set splash screen to load login page

---
+++ Scripts/login_page.gd
@@ -0,0 +1,22 @@
+extends Node2D
+
+@onready var timer: Timer = $Timer
+@onready var progress_bar: ProgressBar = $Button/ProgressBar
+
+func _on_button_button_down() -> void:
+	timer.start()
+
+
+func _on_button_button_up() -> void:
+	timer.stop()
+
+
+func _on_timer_timeout() -> void:
+	LevelManager.load_main_menu()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if timer.is_stopped():
+		progress_bar.value = 0
+	else:
+		progress_bar.value = timer.wait_time - timer.time_left
--- Scripts/splash_screen.gd
+++ Scripts/splash_screen.gd
@@ -1,6 +1,4 @@
 extends Node2D
 
-
-
 func _on_timer_timeout() -> void:
-	LevelManager.load_main_menu()
+	LevelManager.load_login_page()
