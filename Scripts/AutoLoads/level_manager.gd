0506d7842c6ee338eedbf90ff6e1f3fb6b470ea1
David Mulgrew <bigmonmulgrew@users.noreply.github.com>
2025-04-23 09:09:26 +0100

    made static scenes exports and changed teh autoload to have a predefined LevelManager Object

--- Scripts/AutoLoads/level_manager.gd
+++ Scripts/AutoLoads/level_manager.gd
@@ -1,28 +1,56 @@
 extends Node
 
-var reactionTimeLevel: PackedScene
-var memoryLevel: PackedScene
-var focusLevel: PackedScene
-var mentalAgilityLevel: PackedScene
-var decisionMakingLevel: PackedScene
+## Configuration
+@export_category("Default Screens")
+@export var mainMenu: PackedScene
+@export var loginScreen: PackedScene
+@export var userDashboard: PackedScene
+
+# Minigames
+@export_category("Mninigames")
+@export var reactionTimeLevel: PackedScene
+@export var memoryLevel: PackedScene
+@export var focusLevel: PackedScene
+@export var mentalAgilityLevel: PackedScene
+@export var decisionMakingLevel: PackedScene
+
+## Runtime variables
 var current_scene : Node
 
 func load_level(level) -> void:
 	call_deferred("_deferred_load_level", level)
 
 func _deferred_load_level(level):
-	current_scene.free()
+	# Safety check since current_scene will be null on start
+	if current_scene:
+		current_scene.free()
+		
+	var scene_to_instance: PackedScene
 	
-	var new_scene = ResourceLoader.load(level)
-	current_scene = new_scene.instantiate()
+	if typeof(level) == TYPE_STRING:
+		scene_to_instance = ResourceLoader.load(level)
+	elif level is PackedScene:
+		scene_to_instance = level
+	else:
+		push_error("Invalid level type passed to load_level: " + str(level))
+		return
 	
-	get_tree().get_root().add_child(current_scene)
-	get_tree().set_current_scene(current_scene)
+	if scene_to_instance:
+		current_scene = scene_to_instance.instantiate()
+		get_tree().get_root().add_child(current_scene)
+		get_tree().set_current_scene(current_scene)
+	else:
+		push_error("Failed to load or instantiate the scene.")
+
+func load_main_menu():
+	load_level(mainMenu)
+	
+func load_login_page():
+	load_level(loginScreen)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	var root = get_tree().get_root()
-	current_scene = root.get_child(root.get_child_count() - 1)
+	current_scene = get_tree().get_current_scene()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
