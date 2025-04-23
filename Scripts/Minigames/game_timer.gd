c9b7bba8e64d1f54af97b3a3bfbae1e49d6e5fa8
BartholomewGG <Bartholomew.G.G@Gmail.com>
2025-04-23 19:01:30 +0100

    Finished Timer
    Timer now functions properly and counts down from the desired time down to 0 and then stops.
    
    Timer has a setting to allow it to have a custom time to start from.
    
    Timer has an empty function "_on_time_end" that has nothing in it yet that will be attached to future functionality to end minigames and move on to the correct next scene.

--- Scripts/Minigames/game_timer.gd
+++ Scripts/Minigames/game_timer.gd
@@ -1,24 +1,30 @@
 extends Timer
 
-var time_remaining := 120 #Default time for minigames
+@export var time_duration : float = 120 # Time for minigames
+var time_remaining
 @onready var time_text: RichTextLabel = $TimeText
 
 func _ready():
+	time_remaining = time_duration
 	update_Game_Timer()
-	$Timer.start() #Start Timer
-	
-func _on_Timer_timeout():
-	if time_remaining > 0:
-		time_remaining -= 1
-		update_Game_Timer()
-	else:
-		$Timer.stop()
-		time_text.clear()
-		time_text.add_text("0:00")
-		print_debug("Time Up")
+	start() #Start Timer
 	
 func update_Game_Timer():
 	var minutes = time_remaining / 60
 	var seconds = time_remaining % 60
 	time_text.clear()
 	time_text.add_text("%02d:%02d" % [minutes, seconds])
+
+func _on_timeout():
+	if time_remaining > 0:
+		time_remaining -= 1
+		update_Game_Timer()
+	else:
+		stop()
+		time_text.clear()
+		time_text.add_text("00:00")
+		print_debug("Time Up")
+		_on_time_end()
+		
+func _on_time_end(): 
+	pass #TODO: implement function to send signal to end level
