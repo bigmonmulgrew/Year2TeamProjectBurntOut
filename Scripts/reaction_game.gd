fc2962fa4d7a7c9f4e9bbb55b31fecc9ee57c18b
MutantGinga <haydenwilliamhefford@yahoo.com>
2025-04-24 14:15:42 +0100

    Added export vars for magic numbers

--- Scripts/reaction_game.gd
+++ Scripts/reaction_game.gd
@@ -7,8 +7,12 @@ extends Game
 @onready var correct_label = $CorrectIncorrectDisplay/UiCircleLong/Tick/Correct
 @onready var incorrect_label = $CorrectIncorrectDisplay/UiCircleLong/Cross/Incorrect
 
+@export var cycle_timer: float
+@export var win_score: int
+@export var lose_score: int
+
 # Card Textures (loading images)
-var card_textures = {
+var card_textures: Dictionary = {
 	"Spade": preload("res://Images/Sprites/Spade Card.png"),
 	"Diamond": preload("res://Images/Sprites/Diamond Card.png"),
 	"Heart": preload("res://Images/Sprites/Heart Card.png"),
@@ -17,23 +21,23 @@ var card_textures = {
 
 var cards = ["Spade", "Diamond", "Heart", "Club"]
 
-var current_card = ""
-var centre_card_value = ""
-var correct_score = 0
-var incorrect_score = 0
+var current_card: String = ""
+var centre_card_value: String = ""
+var correct_score: int = 0
+var incorrect_score: int = 0
 
-func _ready():
-	timer.wait_time = 1.0
+func _ready() -> void:
+	timer.wait_time = cycle_timer
 	timer.start()
 	pick_new_current_card()
 	print(current_card)
 	#randomize()
 	
-func pick_new_current_card():
+func pick_new_current_card() -> void:
 	current_card = cards[randi() % cards.size()]
 	current_card_sprite.texture = card_textures[current_card]
 
-func show_random_centre_card():
+func show_random_centre_card() -> void:
 	centre_card_value = cards[randi() % cards.size()]
 	centre_card.texture_normal = card_textures[centre_card_value]
 
@@ -41,11 +45,16 @@ func _on_CentreCardButton_pressed() -> void:
 	if centre_card_value == current_card:
 		correct_score += 1
 		correct_label.text = str(correct_score)
-		pick_new_current_card()
+		if correct_score < win_score:
+			pick_new_current_card()
+		else:
+			win_game()
 	else:
 		incorrect_score += 1
 		incorrect_label.text = str(incorrect_score)
-
+		if incorrect_score >= lose_score:
+			# end the game, do we have a method for this already?
+			pass
 
 func _on_centre_card_timer_timeout() -> void:
 	show_random_centre_card()
