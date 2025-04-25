c10c5406b2b79758ba7c755ed580e3d6a4accc5a
MutantGinga <haydenwilliamhefford@yahoo.com>
2025-04-25 15:54:55 +0100

    Employee Feedback Form complete

---
+++ Scripts/Question.gd
@@ -0,0 +1,20 @@
+extends Sprite2D
+
+@onready var buttons: Array = [
+	$StronglyDisagree,
+	$Disagree,
+	$NeitherAgreeOrDisagree,
+	$Agree,
+	$StronglyAgree
+]
+
+var selected_index: int = -1
+
+func _ready():
+	for i in range(buttons.size()):
+		buttons[i].connect("pressed", Callable(self, "_on_button_pressed").bind(i))
+
+func _on_button_pressed(index: int) -> void:
+	selected_index = index
+	for i in range(buttons.size()):
+		buttons[i].set_pressed(i == index)
